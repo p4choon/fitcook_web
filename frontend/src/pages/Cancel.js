d2cf9dae106eff4a5040546d432e7a41e9d2855a
David Gómez Pachón <gomezpachondavid@gmail.com>
2023-05-23 02:25:06 +0200

    Cancel Page acabada

--- frontend/src/pages/Cancel.js
+++ frontend/src/pages/Cancel.js
@@ -1,14 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import './cancel.css';
+
 function Cancel() {
   return (
-    <div>
-      <h1 className="mt-3">Vaya... Has cancelado la compra😢</h1>
-      
-      <a href="/store">
-        <button
-          className="btn btn-primary"
-          style={{ backgroundColor: '#222222', fontWeight: 'bold', color:'white', borderColor:'#FFD700' }}
-        >Volver</button>
-      </a>
+    <div className="cancel-container">
+      <div className="cancel-content">
+        <FiShoppingBag className="cancel-icon" />
+        <h1 className="cancel-title">Vaya... Has cancelado la compra 😢</h1>
+        <p className="cancel-message">¡No te desanimes! Explora nuestra tienda y descubre nuevos productos.</p>
+        <Link to="/store" className="btn cancel-button">
+          Volver a la Tienda
+        </Link>
+      </div>
     </div>
   );
 }
